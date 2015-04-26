e3e736210484426ad72eee7ccb55344df94e416b
ibs-zero <ibs_zero@live.com.mx>
2015-04-25 21:15:18 -0500

    comentarios en main

--- app/src/main/java/com/ticscorp/zero/holamundo/MainActivity.java
+++ app/src/main/java/com/ticscorp/zero/holamundo/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+// hola algunos comentarios
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
